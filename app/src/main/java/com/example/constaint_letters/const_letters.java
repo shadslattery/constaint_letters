429202053d8e265a9e9d22c62ebbacfb2df5b622
shadslattery <shadslattery@gmail.com>
2019-09-16 20:39:40 -0400

    Initial commit

---
+++ app/src/main/java/com/example/constaint_letters/const_letters.java
@@ -0,0 +1,17 @@
+package com.example.constaint_letters;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+    public class const_letters extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(@Nullable Bundle saveInstanceState)
+        {
+            super.onCreate(saveInstanceState);
+            setContentView(R.layout.const_layout);
+        }
+
+    }
